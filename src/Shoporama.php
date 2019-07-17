71b0c98bd58be8b3bf79f27906b351812dd209c5
Stefan Ninic <ninicstefan94@gmail.com>
2019-07-17 16:49:10 +0200

    Removed customers and suppliers

--- src/Shoporama.php
+++ src/Shoporama.php
@@ -2,32 +2,8 @@
 
 namespace KgBot\Shoporama;
 
-
-use KgBot\Shoporama\Builders\CustomerBuilder;
-use KgBot\Shoporama\Builders\CustomerGroupBuilder;
-use KgBot\Shoporama\Builders\CustomerInvoiceBuilder;
-use KgBot\Shoporama\Builders\DraftOrderBuilder;
-use KgBot\Shoporama\Builders\DraftPurchaseOrderBuilder;
-use KgBot\Shoporama\Builders\EmployeeBuilder;
-use KgBot\Shoporama\Builders\FieldBuilder;
-use KgBot\Shoporama\Builders\InventoryAdjustmentBuilder;
-use KgBot\Shoporama\Builders\InventoryMovementBuilder;
-use KgBot\Shoporama\Builders\LocationBuilder;
-use KgBot\Shoporama\Builders\LotBuilder;
 use KgBot\Shoporama\Builders\OrderBuilder;
-use KgBot\Shoporama\Builders\OrderShipmentBuilder;
-use KgBot\Shoporama\Builders\PaymentTermBuilder;
-use KgBot\Shoporama\Builders\PluginBuilder;
 use KgBot\Shoporama\Builders\ProductBuilder;
-use KgBot\Shoporama\Builders\ProductGroupBuilder;
-use KgBot\Shoporama\Builders\ProductionOrderBuilder;
-use KgBot\Shoporama\Builders\PurchaseOrderBuilder;
-use KgBot\Shoporama\Builders\PurchaseOrderReceiptBuilder;
-use KgBot\Shoporama\Builders\SupplierBuilder;
-use KgBot\Shoporama\Builders\SupplierGroupBuilder;
-use KgBot\Shoporama\Builders\SupplierInvoiceBuilder;
-use KgBot\Shoporama\Builders\Variation\VariationBuilder;
-use KgBot\Shoporama\Builders\WebhookBuilder;
 use KgBot\Shoporama\Utils\Request;
 
 class Shoporama
@@ -55,14 +31,6 @@ class Shoporama
     }
 
     /**
-     * @return \KgBot\Shoporama\Builders\SupplierBuilder
-     */
-    public function suppliers()
-    {
-        return new SupplierBuilder($this->request);
-    }
-
-    /**
      * @return \KgBot\Shoporama\Builders\ProductBuilder
      */
     public function products()
@@ -76,14 +44,6 @@ class Shoporama
     public function orders()
     {
         return new OrderBuilder($this->request);
-    }
-
-    /**
-     * @return \KgBot\Shoporama\Builders\CustomerBuilder
-     */
-    public function customers()
-    {
-        return new CustomerBuilder($this->request);
     }
 
     /**
