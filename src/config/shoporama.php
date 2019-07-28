808443106747ed0c7dc6aeaca3be394bcfef9744
Stefan Ninic <ninicstefan94@gmail.com>
2019-07-28 12:38:39 +0200

    Fixed base url in config

--- src/config/shoporama.php
+++ src/config/shoporama.php
@@ -3,5 +3,5 @@
 return [
 
     'token' => env('SHOPORAMA_API_TOKEN'),
-    'base_uri' => env('SHOPORAMA_BASE_URL', 'https://www.shoporama.dk/REST'),
+    'base_uri' => env('SHOPORAMA_BASE_URL', 'https://www.shoporama.dk/REST/'),
 ];
